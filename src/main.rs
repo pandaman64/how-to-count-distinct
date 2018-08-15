56f402b71603cf95bb6f854d47352af798711649
pandaman64 <kointosudesuyo@infoseek.jp>
2018-08-15 14:29:26 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,20 @@
 extern crate diesel;
 extern crate dotenv;
 
-use diesel::prelude::*;
 use diesel::pg::PgConnection;
+use diesel::prelude::*;
 use diesel::sql_types::{Array, Text};
 use dotenv::dotenv;
-use std::env;
 use std::collections::HashSet;
+use std::env;
 
 mod schema;
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
 
 sql_function!(fn unnest(a: Array<Text>) -> Text);
@@ -53,20 +51,22 @@ fn main() {
 
     diesel::insert_into(dsl::t)
         .values(&vec![
-            schema::NewRow { labels: &["foo".to_string(), "bar".to_string()] },
-            schema::NewRow { labels: &["bar".to_string(), "baz".to_string()] },
-        ])
-        .execute(&conn)
+            schema::NewRow {
+                labels: &["foo".to_string(), "bar".to_string()],
+            },
+            schema::NewRow {
+                labels: &["bar".to_string(), "baz".to_string()],
+            },
+        ]).execute(&conn)
         .unwrap();
 
     let mut labels = select_distinct_labels(&conn);
     labels.sort();
 
-    assert_eq!(labels, [
-        "bar".to_string(),
-        "baz".to_string(),
-        "foo".to_string()
-    ]);
+    assert_eq!(
+        labels,
+        ["bar".to_string(), "baz".to_string(), "foo".to_string()]
+    );
 
     // how to implement?
     // assert_eq!(count_distinct_labels(&conn), 4);
